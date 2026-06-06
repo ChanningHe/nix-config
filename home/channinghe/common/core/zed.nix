7a0b6bd7566a849dfab71e7730e9381c5d045c27
Channing He <channinghey@gmail.com>
2026-06-06 21:12:12 +0900

    feat(zed): symlink remote_server binaries recursively into .zed_server

--- home/channinghe/common/core/zed.nix
+++ home/channinghe/common/core/zed.nix
@@ -3,6 +3,6 @@
   home.file.".zed_server" = {
     source = "${pkgs.zed-editor.remote_server}/bin";
     # keeps the folder writable, but symlinks the binaries into it
-    recursive = false;
+    recursive = true;
   };
 }
