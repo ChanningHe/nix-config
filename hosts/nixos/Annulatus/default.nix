36580515f1db048087a73097b17b4edbd8ed1db8
Channing He <channinghey@gmail.com>
2026-06-18 21:43:38 +0900

    feat(hosts): add nixos host Annulatus's config

---
+++ hosts/nixos/Annulatus/default.nix
@@ -0,0 +1,100 @@
+{
+  lib,
+  config,
+  ...
+}:
+let
+  hostNetwork = config.hostSpec.networkInfo.hosts.${config.hostSpec.hostName};
+in
+{
+  imports = lib.flatten [
+    #
+    # ========== Hardware ==========
+    #
+    ./hardware-configuration.nix
+    #inputs.hardware.nixosModules.common-cpu-amd
+    #inputs.hardware.nixosModules.common-cpu-intel
+    #inputs.hardware.nixosModules.common-gpu-nvidia
+    #inputs.hardware.nixosModules.common-gpu-intel
+    #inputs.hardware.nixosModules.common-pc-ssd
+
+    #
+    # ========== Disk Layout ==========
+    #
+    #inputs.disko.nixosModules.disko
+
+    #
+    # ========== Misc Inputs ==========
+    #
+
+    (map lib.custom.relativeToRoot [
+      #
+      # ========== Required Configs ==========
+      #
+      "hosts/common/core"
+
+      #
+      # ========== Non-Primary Users to Create ==========
+      #
+      "hosts/common/optional/system/no-firewall.nix"
+      "hosts/common/optional/system/ip-forward.nix"
+      "hosts/common/optional/services/attic.nix.nix"
+      #
+      # ========== Optional Configs ==========
+      #
+    ])
+  ];
+
+  #
+  # ========== Host Specification ==========
+  #
+
+  hostSpec = {
+    hostName = "Annulatus";
+    scaling = lib.mkForce "1";
+    # loadUserAgeKey = true;
+  };
+
+  #
+  # ========== Host Network ==========
+  #
+  networking = {
+    hostId = "15b9f89f";
+    networkmanager.enable = false;
+    enableIPv6 = false;
+    useDHCP = false;
+    dhcpcd.enable = false;
+    nameservers = hostNetwork.dns;
+  };
+
+  services.resolved.enable = false;
+
+  # systemd-networkd
+  systemd.network = {
+    enable = true;
+    networks = {
+      "10-wired" = {
+        matchConfig.Name = "enp1s0";
+        networkConfig = {
+          Address = [
+            "${hostNetwork.ip4}/24"
+            #"${hostNetwork.ip6}/64"
+          ];
+          Gateway = [
+            "${hostNetwork.gateway4}"
+            #"${hostNetwork.gateway6}"
+          ];
+          DHCP = "no";
+          IPv6AcceptRA = false;
+        };
+      };
+    };
+  };
+
+  boot.initrd = {
+    systemd.enable = true;
+  };
+
+  # https://wiki.nixos.org/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "25.05";
+}
