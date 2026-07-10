3aa15e52af4c53756527f66b68b3d031acae24e3
Channing He <channinghey@gmail.com>
2026-07-10 17:04:20 +0900

    fix(ipmi): run ipmi-exporter as root

--- hosts/common/optional/system/ipmi.nix
+++ hosts/common/optional/system/ipmi.nix
@@ -23,6 +23,7 @@ in
     enable = true;
     listenAddress = cfg.listenAddress or "0.0.0.0";
     port = cfg.port or 29290;
+    user = "root";
     #openFirewall = true;
     configFile = cfg.configFile or null;
     # configFile = cfg.configFile or (pkgs.writeText "ipmi-exporter-local.yml" ''
